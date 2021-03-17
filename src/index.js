5390d7c0e1250646014597e3251bf4c90318a01d
wglee0511 <ranazang@naver.com>
2021-03-17 20:12:52 +0900

    apollo client, gql

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
-
+import client from "./apollo";
 import App from "./components/App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,17 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
 export default function Home(props) {
+  const { loading, data } = useQuery(GET_MOVIES);
+  console.log(loading, data);
   return <h1>Home</h1>;
 }
